266bf692ac5161a7e4a1e095e41323d3d5bfca47
Cyril Bochkarev <podockonnik@gmail.com>
2020-05-19 20:21:58 +0300

    Task 1.22

---
+++ chapter1/22.rkt
@@ -0,0 +1,50 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (inc x)
+  (+ x 1))
+
+(define (sqr x)
+  (* x x))
+
+(define (divides? a b)
+  (= 0 (remainder b a)))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (sqr test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (inc test-divisor)))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (even? n)
+    (= 0 (remainder n 2)))
+
+(define (search-for-primes begin end)
+(define _begin (if (even? begin) (inc begin) begin))
+  (define (iter x)
+    (if (> x end)
+      0
+      ((timed-prime-test x)
+       (iter (+ x 2)))))
+  (iter _begin))
+
+(search-for-primes 1000000 1000100)
